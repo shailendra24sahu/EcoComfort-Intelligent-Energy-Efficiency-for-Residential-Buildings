96e2e209ea275a3bdc075a6723149165e4377fd6
Shailendra Sahu <sahushailendrakumar053@gmail.comgit config --global user.name shailendra24sahu>
2023-07-16 08:22:32 +0530

    Prediction started

---
+++ predictFromModel.py
@@ -0,0 +1,67 @@
+import pandas
+from file_operations import file_methods
+from data_preprocessing import preprocessing
+from data_ingestion import data_loader_prediction
+from application_logging import logger
+from Prediction_Raw_Data_Validation.predictionDataValidation import Prediction_Data_validation
+
+
+
+class prediction:
+
+    def __init__(self,path):
+        self.file_object = open("Prediction_Logs/Prediction_Log.txt", 'a+')
+        self.log_writer = logger.App_Logger()
+        self.pred_data_val = Prediction_Data_validation(path)
+
+    def predictionFromModel(self):
+
+        try:
+            self.pred_data_val.deletePredictionFile() #deletes the existing prediction file from last run!
+            self.log_writer.log(self.file_object,'Start of Prediction')
+            data_getter=data_loader_prediction.Data_Getter_Pred(self.file_object,self.log_writer)
+            data=data_getter.get_data()
+
+
+            preprocessor=preprocessing.Preprocessor(self.file_object,self.log_writer)
+
+            is_null_present,cols_with_missing_values=preprocessor.is_null_present(data)
+            if(is_null_present):
+                data=preprocessor.impute_missing_values(data)
+
+            data  = preprocessor.logTransformation(data)
+
+            
+            #data=data.to_numpy()
+            file_loader=file_methods.File_Operation(self.file_object,self.log_writer)
+            kmeans=file_loader.load_model('KMeans')
+
+            clusters=kmeans.predict(data)
+            data['clusters']=clusters
+            clusters=data['clusters'].unique()
+            result=[] # initialize blank list for storing predicitons
+            # with open('EncoderPickle/enc.pickle', 'rb') as file: #let's load the encoder pickle file to decode the values
+            #     encoder = pickle.load(file)
+
+            for i in clusters:
+                cluster_data= data[data['clusters']==i]
+                cluster_data = cluster_data.drop(['clusters'],axis=1)
+                cluster_data = preprocessor.standardScalingData(cluster_data)
+                principal_cluster_data = preprocessor.principal_components(cluster_data)
+                
+                model_name = file_loader.find_correct_model_file(i)
+                model = file_loader.load_model(model_name)
+                for val in (model.predict(principal_cluster_data.values)):
+                    result.append(val)
+            
+            result = pandas.DataFrame(result,columns=['HL_Prediction','CL_Prediction'])
+            path="Prediction_Output_File/Predictions.csv"
+            result.to_csv("Prediction_Output_File/Predictions.csv",header=True) #appends result to prediction file
+            self.log_writer.log(self.file_object,'End of Prediction')
+        except Exception as ex:
+            self.log_writer.log(self.file_object, 'Error occured while running the prediction!! Error:: %s' % ex)
+            raise ex
+        return path
+
+
+
